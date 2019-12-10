517a73e3ed78957dc37553b8f3a5c39089345aa7
TheEzo <willaschek.t@gmail.com>
2019-12-10 14:10:25 +0100

    task 1

--- src/parallel_builder/loop_mesh_builder.cpp
+++ src/parallel_builder/loop_mesh_builder.cpp
@@ -28,7 +28,8 @@ unsigned LoopMeshBuilder::marchCubes(const ParametricScalarField &field)
     unsigned totalTriangles = 0;
 
     // 2. Loop over each coordinate in the 3D grid.
-    #pragma omp parallel for reduction(+:totalTriangles)
+    // todo static, dynamic, guided vypadaji stejne, auto je pomalsi o sekundu
+    #pragma omp parallel for reduction(+:totalTriangles) schedule(guided,8)
     for(size_t i = 0; i < totalCubesCount; ++i)
     {
         // 3. Compute 3D position in the grid.
@@ -58,6 +59,7 @@ float LoopMeshBuilder::evaluateFieldAt(const Vec3_t<float> &pos, const Parametri
 
     // 2. Find minimum square distance from points "pos" to any point in the
     //    field.
+//    #pragma omp parallel for reduction(min:value)
     for(unsigned i = 0; i < count; ++i)
     {
         float distanceSquared  = (pos.x - pPoints[i].x) * (pos.x - pPoints[i].x);
