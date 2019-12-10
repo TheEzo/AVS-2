0e0aa675e0f1ee6856e77760d13537eaa2293ca0
TheEzo <willaschek.t@gmail.com>
2019-12-10 11:43:38 +0100

    pragmas

--- src/parallel_builder/loop_mesh_builder.cpp
+++ src/parallel_builder/loop_mesh_builder.cpp
@@ -28,7 +28,7 @@ unsigned LoopMeshBuilder::marchCubes(const ParametricScalarField &field)
     unsigned totalTriangles = 0;
 
     // 2. Loop over each coordinate in the 3D grid.
-    #pragma omp reduction(+:totalTriangles)
+    #pragma omp simd reduction(+:totalTriangles)
     for(size_t i = 0; i < totalCubesCount; ++i)
     {
         // 3. Compute 3D position in the grid.
